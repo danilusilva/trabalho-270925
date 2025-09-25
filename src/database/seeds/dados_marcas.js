194ddaddf8d799f5cde3912da607d743a4956e15
danielvalmeida91 <danielvalmeida91@gmail.com>
2025-09-25 19:43:34 -0300

    feat: criando as rotas e consultando no banco de dados

--- src/database/seeds/dados_marcas.js
+++ src/database/seeds/dados_marcas.js
@@ -6,5 +6,6 @@ export async function seed(knex) {
   await knex('marcas').del()
   await knex('marcas').insert([
     {id: 1, nome: 'Apple', site: 'apple.com', telefone: '0800-761-0867'},
+    {id: 2, nome: 'Samsung', site: 'samsung.com', telefone: '0800-761-0087'},
   ]);
 }
--- src/http/routes/index.js
+++ src/http/routes/index.js
@@ -1,4 +1,9 @@
-
+import { database } from '../../database/index.js'
+import { list, listById } from '../controllers/marcas.js'
 export const routes = async(app) => {
-    app.get('/', () => console.log('Olá Mundo2 !!'))
+    app.get('/', (req, res) => {
+        res.status(200).send({ message: "API Ok."})
+    })
+    app.get('/marcas', list)
+    app.get('/marcas/:id', listById)
 }
