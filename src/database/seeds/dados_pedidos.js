0460d4128474571e4d8d023865a8cc62e1577802
Danilu Silva <dan7zada@gmail.com>
2025-09-29 19:18:31 -0300

    Update dados_pedidos.js

--- src/database/seeds/dados_pedidos.js
+++ src/database/seeds/dados_pedidos.js
@@ -3,7 +3,6 @@
  * @returns { Promise<void> }
  */
 export async function seed(knex) {
-  // Deletes ALL existing entries
   await knex("pedidos").del();
   await knex("pedidos").insert([
     { id: 1, data: "2025-09-17", cliente_id: 1, total: 11898 },
